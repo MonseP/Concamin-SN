e89544a3219a965464957c378b4c391b5fd98aec
Miguel Gonzalez <miglez90290995@gmail.com>
2017-11-01 19:20:47 -0600

    New Evento

--- src/components/eventos/EventosContainer.js
+++ src/components/eventos/EventosContainer.js
@@ -1,20 +1,44 @@
 import React, {Component} from 'react';
 import EventsList from './EventsList';
-import {GridList, GridTile} from 'material-ui';
+import {GridList, GridTile, FlatButton, Dialog} from 'material-ui';
 import './EventosStyles.css'
 import FiltrarEventos from "./FiltrarEventos";
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import {FloatingActionButton} from 'material-ui';
+import NuevoEvento from "./NuevoEvento";
 
 class EventosContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            open: false
         };
     }
 
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
     render() {
+        const actions = [
+            <FlatButton
+                label="Cancelar"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+            <FlatButton
+                label="Agregar"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.handleClose}
+            />,
+        ];
         const {fetched, eventos} = this.props;
         if(fetched){
             console.log(eventos);
@@ -31,6 +55,20 @@ class EventosContainer extends Component {
                                 <EventsList eventos={eventos}/>
                             </GridTile>
                         </GridList>
+                        <Dialog
+                            title="Crear nuevo evento"
+                            actions={actions}
+                            modal={false}
+                            open={this.state.open}
+                            onRequestClose={this.handleClose}
+                        >
+                            <NuevoEvento/>
+                        </Dialog>
+                        <FloatingActionButton
+                            onClick={this.handleOpen}
+                            className="fab-button">
+                            <ContentAdd/>
+                        </FloatingActionButton>
                     </div>
                     :
                     <div className="root-eventos">
