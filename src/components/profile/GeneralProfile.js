8321ad04d26407f9a05636f6f63f37c4a12fdb7b
BlisS <contacto@fixter.org>
2017-12-15 18:24:29 -0600

    profile

--- src/components/profile/GeneralProfile.js
+++ src/components/profile/GeneralProfile.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {Paper} from 'material-ui';
+import FontAwesome from 'react-fontawesome';
+import {Link} from 'react-router-dom';
 
 const bliss = "https://media-exp2.licdn.com/mpr/mpr/shrinknp_400_400/AAIAAQDGAAwAAQAAAAAAAAt1AAAAJDg0YWZkZDNiLTIwZTYtNGQyYy04ZWE5LWE0YmQwODdjMWZjNQ.jpg";
 
@@ -15,13 +17,15 @@ class GeneralProfile extends Component {
                 <Paper className="paper" zDepth={3}>
                     <div className="general-profile-cover">
                         <img src={bliss} alt="user"/>
-
                     </div>
                     <div className="general-profile-info">
                         <h2>Héctor BlisS</h2>
                         <p>FullStack developer</p>
                         <p>Google developer groups</p>
                         <p>Pachuca de soto</p>
+                        <Link to={"/profile/basic/edit"}>
+                        <FontAwesome name={"ellipsis-h"} />
+                        </Link>
                     </div>
                 </Paper>
 
